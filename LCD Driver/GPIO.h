7d4adb95fe8c17c5903d68b897888be34668b65a
KareemMamdouhHassan <104162336+KareemMamdouhHassan@users.noreply.github.com>
2022-05-19 11:59:17 +0200

    Delete LCD Driver directory

--- LCD Driver/GPIO.h
+++
@@ -1,3 +0,0 @@
-#include "tm4c123gh6pm.h"
-
-void PORT_Init(void);
--- LCD Driver/SysTick.h
+++
@@ -1,5 +0,0 @@
-#include "tm4c123gh6pm.h"
-
-void SysTick_Init(void);
-void SysTick_wait(unsigned long delay);
-void SysTick_wait10ms(unsigned long delay);
